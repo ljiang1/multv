2d4a35aaba6503b3587e53ed879ff9811b038a33
Liying Jiang <ljiang@splunk.com>
2019-11-07 11:03:37 -0800

    add sdk client

---
+++ sdk/client.go
@@ -0,0 +1,12 @@
+package my
+
+import (
+	"fmt"
+	"github.com/ljiang1/multv/ac_v4"
+)
+
+func NewClient() {
+
+	fmt.Println("sdk client call action")
+	my.NewStuff()
+}
---
+++ sdk/go.mod
@@ -0,0 +1,5 @@
+module github.com/ljiang1/multv/sdk
+
+go 1.12
+
+require github.com/ljiang1/multv/ac_v4 v0.0.0-20191107185813-79e50ee02ddb
